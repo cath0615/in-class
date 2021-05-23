192ec24af5053535aed5c83b5326de603d45b861
Catherine Miao <md0615@uw.edu>
2021-05-22 21:43:36 -0700

    complete demo-16

--- day-16/src/App.js
+++ day-16/src/App.js
@@ -4,11 +4,25 @@ import { HashRouter as Router, Route, Link } from "react-router-dom";
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 // Import your other components here
+import { Homepage } from './Homepage';
+import { About } from './About';
+import { Projects } from './Projects';
 
 class App extends Component {
     render() {
         return (<div>
-                  { /*Render your Router, Links, and Routes in here */ }
+                    <Router> 
+                        <div className="container"> 
+                            <Link to="/">Home</Link> 
+                            { ' - ' }
+                            <Link to="/projects">Projects</Link> 
+                            { ' - ' }
+                            <Link to="/about">About</Link>
+                            <Route exact path="/" component={ Homepage } /> 
+                            <Route path="/projects" component={ Projects } /> 
+                            <Route path="/about" component={ About } /> 
+                        </div>
+                    </Router>
                 </div>
             );
     }
